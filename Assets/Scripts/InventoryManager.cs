9759a0b4997b9ee5248e68e13cd538d87c64ade3
Ben <bencostrell@gmail.com>
2019-01-08 22:23:09 -0500

    bit more inventory framework

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    private InventorySlot[] inventorySlots;
+    
+    void Awake()
+    {
+        inventorySlots = GetComponentsInChildren<InventorySlot>();
+    }
+
+    private void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySlot : MonoBehaviour
+{
+    private Image slotImage;
+    private TextMeshProUGUI countText;
+
+    void Awake()
+    {
+        slotImage = GetComponentsInChildren<Image>()[1];
+        countText = GetComponentInChildren<TextMeshProUGUI>();
+        countText.enabled = false;
+    }
+
+    public void SetSlot(Sprite sprite, int count, bool on = true)
+    {
+        slotImage.enabled = on;
+        countText.enabled = on && count != 0;
+        slotImage.sprite = sprite;
+        countText.text = count.ToString();
+    }
+}
